55b58f8fbe1afc6d21ab9f19abbaeb17eb589f3b
girishkdeshpande <girish.deshpande@mindbowser.com>
2024-02-02 11:16:03 +0530

    corrections done

--- panda_plotting2.py
+++ panda_plotting2.py
@@ -2,35 +2,43 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-telcom = pd.read_csv('D:/data/telcom1.csv')
-
 
 def data_check_for_null():
     # Load the data and check if it consist of any missing data or not?
-    # Stack chart
+    # Horizontal Bar chart
+    telcom = pd.read_csv('D:/data/telcom1.csv')
+
     plt.style.use('classic')
 
-    missing_values = pd.DataFrame(telcom.head(), columns=['state', 'account length', 'area code', 'phone number', 'international plan'])
-    missing_values.isnull().any()
-
-    cols = [col for col in missing_values.columns if missing_values[col].isnull().any()]
-    missing_count = missing_values.isna().sum()
-    plt.bar(missing_count.index, missing_count.values, color='r')
-    plt.ylim([0, 5])
+    # missing_values = pd.DataFrame(telcom.head(), columns=['state', 'account length', 'area code', 'phone number', 'international plan'])
+    telcom.isnull().any()
+    # nn_cols = [col for col in missing_values.columns if missing_values[col].notnull().any()]
+    not_missing_count = telcom.notna().sum()
+    # cols = [col for col in missing_values.columns if missing_values[col].isnull().any()]
+    missing_count = telcom.isna().sum()
+    y_indexes = np.array([0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40])
+    width = 0.5
+    plt.barh(y_indexes, missing_count.values, color='r', label='Values missing')
+    plt.barh(y_indexes+width, not_missing_count.values, color='b', label='Values not missing')
+    plt.xlim([0, 10])
     plt.title('Missing data')
-    plt.xlabel('Columns')
-    plt.ylabel('Count of missing values')
+    plt.xlabel('Count of missing values')
+    plt.ylabel('Columns')
+    plt.yticks(ticks=y_indexes+width, labels=missing_count.index)
     plt.tight_layout()
+    plt.legend()
     plt.show()
+
+
+telcom = pd.read_csv('D:/data/telcom.csv')
 
 
 def state_wise_distribution():
     # Show state wise distribution
-    # Scatter chart
+    # Stem chart
     plt.style.use('classic')
 
     state_wise_distr = telcom['state'].value_counts()
-    # print('State wise distribution :\n', state_wise_distr)
 
     plt.stem(state_wise_distr.index, state_wise_distr.values)
     plt.title('State Wise Distribution')
@@ -40,52 +48,25 @@ def state_wise_distribution():
     plt.show()
 
 
-
 def call_distribution_churn_wise():
     # Show Churn wise distribution of the calls for morning, evening & night calls
-    # Pie chart
+    # Line chart
     plt.style.use('ggplot')
 
     churn_f = telcom.loc[~telcom.churn]
-    morning_call_series_f = churn_f['total day calls'].values
-    evening_call_series_f = churn_f['total eve calls'].values
-    night_call_series_f = churn_f['total night calls'].values
-    total_morning_calls_f, total_evening_calls_f, total_night_calls_f = 0, 0, 0
-    for morn in morning_call_series_f:
-        total_morning_calls_f += morn
-
-    for eve in evening_call_series_f:
-        total_evening_calls_f += eve
-
-    for nigt in night_call_series_f:
-        total_night_calls_f += nigt
-
-    churn_f_data = [total_morning_calls_f, total_evening_calls_f, total_night_calls_f]
-    # print('Total Churn False call wise data', churn_f_data)
+    morning_calls_f = churn_f['total day calls'].sum()
+    evening_calls_f = churn_f['total eve calls'].sum()
+    night_calls_f = churn_f['total night calls'].sum()
 
     churn_t = telcom.loc[telcom.churn]
-    morning_call_series_t = churn_t['total day calls'].values
-    evening_call_series_t = churn_t['total eve calls'].values
-    night_call_series_t = churn_t['total night calls'].values
-    total_morning_calls_t, total_evening_calls_t, total_night_calls_t = 0, 0, 0
-    for morn in morning_call_series_t:
-        total_morning_calls_t += morn
+    morning_calls_t = churn_t['total day calls'].sum()
+    evening_calls_t = churn_t['total eve calls'].sum()
+    night_calls_t = churn_t['total night calls'].sum()
 
-    for eve in evening_call_series_t:
-        total_evening_calls_t += eve
-
-    for nigt in night_call_series_t:
-        total_night_calls_t += nigt
-
-    churn_t_data = [total_morning_calls_t, total_evening_calls_t, total_night_calls_t]
-    # print('Total Churn True call wise data', churn_t_data)
-
-    # fig, ax = plt.subplots(sharex=True)
     fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
-    slices1 = [churn_f_data[0], churn_f_data[1], churn_f_data[2]]
-    slices2 = [churn_t_data[0], churn_t_data[1], churn_t_data[2]]
+    slices1 = [morning_calls_f, evening_calls_f, night_calls_f]
+    slices2 = [morning_calls_t, evening_calls_t, night_calls_t]
     labels1 = ['Morning', 'Evening', 'Night']
-    # x_indexes = np.arange(len(labels1))
 
     ax1.plot(labels1, slices1, color='k', label='Churn = False')
     ax1.set_title('Churn wise distribution of the calls for morning, evening & night calls')
@@ -97,21 +78,13 @@ def call_distribution_churn_wise():
     ax2.set_ylabel('Total calls')
     ax2.legend(loc='upper right')
 
-    # plt.plot(labels1, slices1, color='k')
-    # plt.plot(labels1, slices2, color='b')
-    #
-    # plt.title('Churn wise distribution of the calls for morning, evening & night calls')
-    # plt.xlabel('Call Time Period')
-    # plt.ylabel('Total calls')
-    # plt.ylim([20000, 300000])
-    # plt.xticks(ticks=x_indexes, labels=labels1)
     plt.tight_layout()
-    # plt.legend(loc='upper right')
     plt.show()
 
 
 def voicemail_count_by_churn():
     # Count of voicemail plan by churn
+    # Bar chart
     plt.style.use('ggplot')
 
     churn_f_voicemail = telcom.loc[~telcom.churn]
@@ -122,12 +95,12 @@ def voicemail_count_by_churn():
 
     fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
 
-    ax1.bar(voicemail_f_count.index, voicemail_f_count.values, color='b', label='Churn = False')
+    ax1.bar(voicemail_f_count.index, voicemail_f_count.values, width=0.25, color='b', label='Churn = False')
     ax1.set_title('Count of voicemail plan by churn')
     ax1.set_ylabel('Voice-mail Count')
     ax1.legend()
 
-    ax2.bar(voicemail_t_count.index, voicemail_t_count.values, color='r', label='Churn = True')
+    ax2.bar(voicemail_t_count.index, voicemail_t_count.values, width=0.25, color='r', label='Churn = True')
     ax2.set_xlabel('Voice mail Plan')
     ax2.set_ylabel('Voice-mail Count')
     ax2.legend()
@@ -141,18 +114,15 @@ def top_5_states_account_length_wise():
     # stem chart
     plt.style.use('ggplot')
 
-    state_wise_top_5 = telcom[['state', 'account length']].sort_values(by='account length', ascending=False)
+    # state_wise_top_5 = telcom[['state', 'account length']].sort_values(by='account length', ascending=False)
     # print('Top 5 states account length wise :\n', state_wise_top_5.values[0:5, 0], state_wise_top_5.values[0:5, 1])
+    # x_axis = state_wise_top_5.values[0:5, 0]
+    # y_axis = state_wise_top_5.values[0:5, 1]
 
-    # length_wise_top_5 = telcom[['state', 'account length']]
-    # length_wise = length_wise_top_5.groupby('state')['account length'].value_counts(ascending=True)
-    # print('length wise:\n', length_wise)
-    # # print('Length wise values:\n', length_wise.values)
+    length_wise = telcom.groupby('state')['account length'].sum()
+    top_5 = length_wise.sort_values(ascending=False).head()
 
-    x_axis = state_wise_top_5.values[0:5, 0]
-    y_axis = state_wise_top_5.values[0:5, 1]
-
-    plt.stem(x_axis, y_axis)
+    plt.stem(top_5.index, top_5.values)
     plt.title('Top 5 states account length wise')
     plt.xlabel('States')
     plt.ylabel('Account Length')
@@ -162,26 +132,17 @@ def top_5_states_account_length_wise():
 
 def total_call_charges():
     # Total charge by morning, evening & night
+    # Horizontal Bar chart
     plt.style.use('seaborn-v0_8-dark')
 
-    morning_charge_series = telcom['total day charge']
-    evening_charge_series = telcom['total eve charge']
-    night_charge_series = telcom['total night charge']
-    morning_charge, evening_charge, night_charge = 0, 0, 0
-    for morn in morning_charge_series:
-        morning_charge += morn
-        morning_charge = round(morning_charge, 2)
+    morning_charge = telcom['total day charge'].sum()
+    morning_charge = round(morning_charge, 2)
 
-    for eve in evening_charge_series:
-        evening_charge += eve
-        evening_charge = round(evening_charge, 2)
+    evening_charge = telcom['total eve charge'].sum()
+    evening_charge = round(evening_charge, 2)
 
-    for nigt in night_charge_series:
-        night_charge += nigt
-        night_charge = round(night_charge, 2)
-
-    # total_charge = [morning_charge, evening_charge, night_charge]
-    # print('Total Charge: ', total_charge)
+    night_charge = telcom['total night charge'].sum()
+    night_charge = round(night_charge, 2)
 
     plt.barh(['Morning'], morning_charge, label='Morning', color='k')
     plt.barh(['Evening'], evening_charge, label='Evening', color='r')
